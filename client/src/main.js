0a1acc2e7fc21bb1895769cd7a08863738ec3276
Victor Tolbert <victor.tolbert@gmail.com>
2021-06-22 22:32:17 -0400

    wip

--- client/src/main.js
+++ client/src/main.js
@@ -27,29 +27,28 @@ Vue.directive('NodeIntersect', NodeIntersect)
 // inject global axios instance as http client to Model
 Model.$http = axios
 
-
 new Vue({
   i18n,
   router,
   store,
   render: h => h(App),
-  created() {
-    const userString = localStorage.getItem('user')
-    if (userString) {
-      const userData = JSON.parse(userString)
-      this.$store.commit('SET_USER_DATA', userData)
-    }
-    //
-    axios.interceptors.response.use(
-      response => response,
-      error => {
-        console.log(error.response)
-        if (error.response.status === 401) {
-          this.$router.push('/')
-          this.$store.dispatch('logout')
-        }
-        return Promise.reject(error)
-      },
-    )
-  },
+  // created() {
+  //   const userString = localStorage.getItem('user')
+  //   if (userString) {
+  //     const userData = JSON.parse(userString)
+  //     this.$store.commit('SET_USER_DATA', userData)
+  //   }
+  //   //
+  //   axios.interceptors.response.use(
+  //     response => response,
+  //     error => {
+  //       console.log(error.response)
+  //       if (error.response.status === 401) {
+  //         this.$router.push('/')
+  //         this.$store.dispatch('logout')
+  //       }
+  //       return Promise.reject(error)
+  //     },
+  //   )
+  // },
 }).$mount('#app')
